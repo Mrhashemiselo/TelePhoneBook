8f3ae018461b3cdb846fd565aa7757e03fb9d647
Mohammadreza Hashemi <1160302014@edc.local>
2024-02-17 11:18:04 +0330

    Add project files.

---
+++ Domain/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Domain
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDomain(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
---
+++ Security/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Security
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddSecurity(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
